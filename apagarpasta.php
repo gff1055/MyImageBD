520bbb3f3fc7b46e84ee4edb4af3a50cc9e36c66
gff1055 <guilhermeferreirafb@gmail.com>
2018-11-29 10:39:22 -0200

    teste criacao de pastas

--- apagarpasta.php
+++ apagarpasta.php
@@ -1,16 +1,15 @@
 <?php
 $pasta = "arquivos3/";
 
-if(is_dir($pasta)){
-	$diretorio = dir($pasta);
-	while($arquivo = $diretorio->read()){
+if(is_dir($pasta)){	// verifica se o caminho colocado é um diretorio
+	$diretorio = dir($pasta);	// variavel diretorio recebe a instancia 'diretorio' associada ao caminho colocado
+	while($arquivo = $diretorio->read()){	// lendo cada um dos arquvos do diretorio
 		if(($arquivo != '.') && ($arquivo != '..')){
-			unlink($pasta.$arquivo);
-			echo 'Arquivo '.$arquivo.' foi apagado com sucesso. <br />';
+			unlink($pasta.$arquivo);	// apagando o arquivo da pasta
+			echo 'Arquivo '.$arquivo.' foi apagado com sucesso. <br />';	// exibindo confimacao
 		}
 	}
-
-	$diretorio->close();
+	$diretorio->close();	// fechando o diretorio
 }
 else{
 	echo 'A pasta não existe.';
@@ -18,10 +17,10 @@ else{
 ?>
 
 <?php
-$pasta = 'arquivos3';
+$pasta = 'arquivos3';	// caminho dapasta
 
-if(rmdir($pasta)){
-	echo 'Pasta deletada com sucesso.';
+if(rmdir($pasta)){	// removendo a pasta
+	echo 'Pasta deletada com sucesso.';	// mensagem exibida caso a pasta tenha sido excluida
 }
 
 else{
